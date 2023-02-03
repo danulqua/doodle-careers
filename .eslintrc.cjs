63586a0b73431934f676985984e998417e65ddbb
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-03 13:06:06 +0200

    update configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,12 @@ module.exports = {
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-prettier',
+    'plugin:vitest/recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
+  },
+  env: {
+    'vitest-globals': true,
   },
 };
