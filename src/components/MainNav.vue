8e6183b975a51200bc54bacc1bd661b4f38726da
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-03 16:24:39 +0200

    add test for navigation links fix styles

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,7 +3,20 @@
     <div
       class="flex h-full items-center border-b border-solid border-brand-gray-1 px-8"
     >
-      <a href="/" class="text-xl text-black">danulqua Careers</a>
+      <a href="/" class="flex h-full items-center text-xl text-black"
+        >danulqua Careers</a
+      >
+      <nav class="ml-12 h-full">
+        <ul class="flex h-full items-center space-x-6">
+          <li
+            v-for="item in links"
+            :key="item.text"
+            class="flex h-full items-center"
+          >
+            <a :href="item.to" class="py-2.5 text-sm">{{ item.text }}</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 </template>
@@ -11,5 +24,35 @@
 <script>
 export default {
   name: 'MainNav',
+  data() {
+    return {
+      links: [
+        {
+          text: 'Teams',
+          to: '#',
+        },
+        {
+          text: 'Locations',
+          to: '#',
+        },
+        {
+          text: 'Life at danulqua Corp',
+          to: '#',
+        },
+        {
+          text: 'How we hire',
+          to: '#',
+        },
+        {
+          text: 'Students',
+          to: '#',
+        },
+        {
+          text: 'Jobs',
+          to: '#',
+        },
+      ],
+    };
+  },
 };
 </script>
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -8,4 +8,18 @@ describe('MainNav', () => {
     const company = screen.getByText('danulqua Careers');
     expect(company).toBeInTheDocument();
   });
+
+  it('displays menu link items', () => {
+    render(MainNav);
+    const navigationLinks = screen.getAllByRole('listitem');
+    const navigationTexts = navigationLinks.map((item) => item.textContent);
+    expect(navigationTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at danulqua Corp',
+      'How we hire',
+      'Students',
+      'Jobs',
+    ]);
+  });
 });
