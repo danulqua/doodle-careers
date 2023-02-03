4788a344dd4b4e9613b0ccfe0faea7864fc5a156
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-03 11:35:19 +0200

    add link to nav component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,11 @@
 <template>
-  <header class="text-sm">Main nav</header>
+  <header class="fixed top-0 left-0 h-16 w-full bg-white">
+    <div
+      class="flex h-full items-center border-b border-solid border-brand-gray-1 px-8"
+    >
+      <a href="/" class="text-xl text-black">danulqua Careers</a>
+    </div>
+  </header>
 </template>
 
 <script>
