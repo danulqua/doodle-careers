428e7e244ce7877256d40adde9a20a718249b432
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-05 11:25:51 +0200

    create common text input component

---
+++ src/components/common/TextInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <input
+    type="text"
+    :value="modelValue"
+    class="h-full w-full text-lg font-normal focus:outline-none"
+    @input="handleInputChange"
+  />
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  // emits: ['update:modelValue'],
+  methods: {
+    handleInputChange($event) {
+      this.$emit('update:modelValue', $event.target.value);
+    },
+  },
+};
+</script>
