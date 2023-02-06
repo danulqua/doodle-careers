b7454de34367a78074daab47aa529ced22d5a620
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-06 22:26:11 +0200

    add reusable function in sub nav tests that accepts route name

--- tests/unit/components/navigation/Subnav.test.js
+++ tests/unit/components/navigation/Subnav.test.js
@@ -3,21 +3,24 @@ import { render, screen } from '@testing-library/vue';
 import Subnav from '@/components/navigation/Subnav.vue';
 
 describe('Subnav', () => {
+  const renderSubnav = (routeName) => {
+    const $route = {
+      name: routeName,
+    };
+
+    render(Subnav, {
+      global: {
+        mocks: { $route },
+        stubs: {
+          FaIcon: true,
+        },
+      },
+    });
+  };
+
   describe('when user is on the jobs page', () => {
     it('renders the job search results', () => {
-      const $route = {
-        name: 'JobsResults',
-      };
-
-      render(Subnav, {
-        global: {
-          mocks: { $route },
-          stubs: {
-            FaIcon: true,
-          },
-        },
-      });
-
+      renderSubnav('JobsResults');
       const jobsCounts = screen.getByText('2758');
       expect(jobsCounts).toBeInTheDocument();
     });
@@ -25,24 +28,7 @@ describe('Subnav', () => {
 
   describe('when user is NOT on the jobs page', () => {
     it('does not render the job search results', () => {
-      const $route = {
-        name: 'Home',
-      };
-
-      render(Subnav, {
-        global: {
-          mocks: { $route },
-          stubs: {
-            FaIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobsPage: false,
-          };
-        },
-      });
-
+      renderSubnav('Home');
       const jobsCounts = screen.queryByText('2758');
       expect(jobsCounts).not.toBeInTheDocument();
     });
