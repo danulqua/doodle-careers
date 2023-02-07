ebcea859993c1bd592f571f811c15a434929a275
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-07 19:24:31 +0200

    add api request to get job listings

--- src/components/job-results/JobListings.vue
+++ src/components/job-results/JobListings.vue
@@ -1,20 +1,27 @@
 <template>
   <main class="flex-auto bg-brand-gray-2 p-8">
     <ol>
-      <JobListing />
-      <JobListing />
-      <JobListing />
-      <JobListing />
-      <JobListing />
+      <JobListing v-for="job in jobs" :key="job.id" :job="job" />
     </ol>
   </main>
 </template>
 
 <script>
+import axios from 'axios';
 import JobListing from '@/components/job-results/JobListing.vue';
 
 export default {
   name: 'JobListings',
   components: { JobListing },
+  data() {
+    return {
+      jobs: [],
+    };
+  },
+  mounted() {
+    axios.get('http://localhost:3000/jobs').then((res) => {
+      this.jobs = res.data;
+    });
+  },
 };
 </script>
