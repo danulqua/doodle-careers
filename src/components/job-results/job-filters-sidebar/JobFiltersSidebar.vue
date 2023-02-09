b8c2d5a76470d40b63143d6095e95f687183fffc
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-09 15:28:26 +0200

    add collapsible accordion component

--- src/components/job-results/job-filters-sidebar/JobFiltersSidebar.vue
+++ src/components/job-results/job-filters-sidebar/JobFiltersSidebar.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="flex w-96 flex-col border-r border-brand-gray-1 bg-white p-4">
-    Filters sidebar
+    <section class="pb-5">
+      <div class="flex justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <ActionButton text="Clear filters" type="secondary" />
+        </div>
+      </div>
+
+      <CollapsibleAccordion />
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from '@/components/common/ActionButton.vue';
+import CollapsibleAccordion from '@/components/common/CollapsibleAccordion.vue';
+
 export default {
   name: 'JobFiltersSidebar',
+  components: { ActionButton, CollapsibleAccordion },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,19 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSearch,
+  faAngleDown,
+  faAngleUp,
+} from '@fortawesome/free-solid-svg-icons';
 
 import '@/index.css';
 import router from '@/router';
 import App from '@/App.vue';
 
 library.add(faSearch);
+library.add(faAngleDown);
+library.add(faAngleUp);
 
 const pinia = createPinia();
 
