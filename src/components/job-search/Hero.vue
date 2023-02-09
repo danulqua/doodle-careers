02241d7acf64668c43dffdb0ddb14e8d1cb393c4
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-09 21:45:30 +0200

    add spotlight component with scoped slots

--- src/components/job-search/Hero.vue
+++ src/components/job-search/Hero.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <section class="flex h-screen flex-col pt-10 pb-20">
+  <main class="flex h-screen flex-col">
+    <section class="pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-span-5 col-start-2">
           <Headline @change="handleActionChange" />
@@ -11,6 +11,32 @@
         </div>
       </div>
     </section>
+
+    <SpotLight class="flex justify-center pb-16">
+      <template #default="{ img, title, description }">
+        <RouterLink
+          to="/jobs/results"
+          class="mx-5 flex h-96 w-72 flex-col overflow-hidden rounded-lg border bg-brand-gray-2"
+        >
+          <img :src="img" :alt="title" class="object-contain" />
+          <div class="mt-3 h-48 px-6 py-4">
+            <h3 class="text-lg font-medium">
+              {{ title }}
+            </h3>
+
+            <p class="mt-3 text-sm">
+              {{ description }}
+            </p>
+          </div>
+          <RouterLink
+            to="/jobs/results"
+            class="px-6 pb-4 text-sm text-brand-blue-1"
+          >
+            See jobs
+          </RouterLink>
+        </RouterLink>
+      </template>
+    </SpotLight>
   </main>
 </template>
 
@@ -18,10 +44,11 @@
 import Headline from '@/components/job-search/Headline.vue';
 import JobSearchForm from '@/components/job-search/JobSearchForm.vue';
 import HeroImages from '@/components/job-search/HeroImages.vue';
+import SpotLight from '@/components/job-search/SpotLight.vue';
 
 export default {
   name: 'Hero',
-  components: { Headline, JobSearchForm, HeroImages },
+  components: { Headline, JobSearchForm, HeroImages, SpotLight },
   data() {
     return {
       activeIdx: 0,
---
+++ src/components/job-search/SpotLight.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="spotlight in spotlights" :key="spotlight.id">
+      <slot
+        :img="spotlight.img"
+        :title="spotlight.title"
+        :description="spotlight.description"
+      ></slot>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'SpotLight',
+  data() {
+    return {
+      spotlights: [],
+    };
+  },
+  mounted() {
+    const baseUrl = import.meta.env.VITE_APP_API_URL;
+    const url = `${baseUrl}/spotlights`;
+
+    axios.get(url).then((response) => (this.spotlights = response.data));
+  },
+};
+</script>
