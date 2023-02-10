b41b10e3007a21c6bbf822a9bd641f557a33a7e3
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-10 20:44:27 +0200

    update count of the matched jobs

--- src/components/navigation/Subnav.vue
+++ src/components/navigation/Subnav.vue
@@ -2,15 +2,31 @@
   <div class="flex h-16 items-center border-b border-brand-gray-1 px-8 text-sm">
     <div v-if="onJobsResultsPage">
       <fa-icon :icon="['fas', 'search']" class="mr-3" />
-      <span><span class="text-brand-green-1">2758</span> jobs found</span>
+      <span
+        ><span class="text-brand-green-1">{{
+          FILTERED_JOBS_BY_ORGANIZATIONS.length
+        }}</span>
+        {{ countLabel }} found</span
+      >
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'pinia';
+import { useJobsStore, FILTERED_JOBS_BY_ORGANIZATIONS } from '@/stores/jobs';
+
 export default {
   name: 'Subnav',
   computed: {
+    ...mapState(useJobsStore, {
+      FILTERED_JOBS_BY_ORGANIZATIONS,
+      countLabel() {
+        return this.FILTERED_JOBS_BY_ORGANIZATIONS.length === 1
+          ? 'job'
+          : 'jobs';
+      },
+    }),
     onJobsResultsPage() {
       return this.$route.name === 'JobsResults';
     },
--- tests/unit/components/navigation/Subnav.test.js
+++ tests/unit/components/navigation/Subnav.test.js
@@ -1,36 +1,52 @@
 import { render, screen } from '@testing-library/vue';
+import { createTestingPinia } from '@pinia/testing';
 
 import Subnav from '@/components/navigation/Subnav.vue';
+import { useJobsStore } from '@/stores/jobs';
 
 describe('Subnav', () => {
-  const renderSubnav = (routeName) => {
+  function renderSubnav(routeName) {
+    const pinia = createTestingPinia();
+    const jobsStore = useJobsStore();
+
     const $route = {
       name: routeName,
     };
 
     render(Subnav, {
       global: {
+        plugins: [pinia],
         mocks: { $route },
         stubs: {
           FaIcon: true,
         },
       },
     });
-  };
+
+    return { jobsStore };
+  }
 
   describe('when user is on the jobs page', () => {
-    it('renders the job search results', () => {
-      renderSubnav('JobsResults');
-      const jobsCounts = screen.getByText('2758');
-      expect(jobsCounts).toBeInTheDocument();
+    it('renders the job search results', async () => {
+      const { jobsStore } = renderSubnav('JobsResults');
+
+      const jobsCount = 21;
+      jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS = Array(jobsCount).fill({});
+
+      const value = await screen.findByText(jobsCount);
+      expect(value).toBeInTheDocument();
     });
   });
 
   describe('when user is NOT on the jobs page', () => {
-    it('does not render the job search results', () => {
-      renderSubnav('Home');
-      const jobsCounts = screen.queryByText('2758');
-      expect(jobsCounts).not.toBeInTheDocument();
+    it('does not render the job search results', async () => {
+      const { jobsStore } = renderSubnav('Home');
+
+      const jobsCount = 21;
+      jobsStore.FILTERED_JOBS_BY_ORGANIZATIONS = Array(jobsCount).fill({});
+
+      const value = screen.queryByText(jobsCount);
+      expect(value).not.toBeInTheDocument();
     });
   });
 });
