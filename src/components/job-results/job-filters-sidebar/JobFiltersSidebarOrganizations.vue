2fe083e94e72058c5e91526bcd7b4bd1791608c2
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-10 17:48:21 +0200

    use unique orgs set in filters component

--- src/components/job-results/job-filters-sidebar/JobFiltersSidebarOrganizations.vue
+++ src/components/job-results/job-filters-sidebar/JobFiltersSidebarOrganizations.vue
@@ -3,25 +3,15 @@
     <div class="mt-5">
       <fieldset>
         <ul class="flex flex-wrap">
-          <li class="h-8 w-1/2">
-            <input type="checkbox" id="VueTube" class="mr-3" />
-            <label for="VueTube">VueTube</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input type="checkbox" id="VueTube" class="mr-3" />
-            <label for="VueTube">VueTube</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input type="checkbox" id="VueTube" class="mr-3" />
-            <label for="VueTube">Halfman is our everything</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input type="checkbox" id="VueTube" class="mr-3" />
-            <label for="VueTube">VueTube</label>
-          </li>
-          <li class="h-8 w-1/2">
-            <input type="checkbox" id="VueTube" class="mr-3" />
-            <label for="VueTube">VueTube</label>
+          <li
+            v-for="organization in UNIQUE_ORGANIZATIONS"
+            :key="organization"
+            class="flex min-h-[32px] w-1/2 items-baseline space-x-2"
+          >
+            <input type="checkbox" :id="organization" class="cursor-pointer" />
+            <label :for="organization" class="h-full cursor-pointer pr-4">
+              {{ organization }}
+            </label>
           </li>
         </ul>
       </fieldset>
@@ -30,10 +20,16 @@
 </template>
 
 <script>
+import { mapState } from 'pinia';
+import { UNIQUE_ORGANIZATIONS, useJobsStore } from '@/stores/jobs';
+
 import CollapsibleAccordion from '@/components/common/CollapsibleAccordion.vue';
 
 export default {
   name: 'JobFiltersSidebarOrganizations',
   components: { CollapsibleAccordion },
+  computed: {
+    ...mapState(useJobsStore, [UNIQUE_ORGANIZATIONS]),
+  },
 };
 </script>
