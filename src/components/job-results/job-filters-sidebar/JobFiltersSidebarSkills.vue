e5b77870a693e6c0bceac7f2b0110627d85a0f74
Danil Zalizchuk <danilmail0110@gmail.com>
2023-02-20 20:46:40 +0200

    use get and set in computed to avoid local data

--- src/components/job-results/job-filters-sidebar/JobFiltersSidebarSkills.vue
+++ src/components/job-results/job-filters-sidebar/JobFiltersSidebarSkills.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="mt-2">
     <input
+      v-model.lazy.trim="skillsSearchTerm"
       type="text"
       class="h-12 w-full rounded border border-brand-gray-1 p-3 text-base shadow-gray"
       placeholder="Computer programming, Finance degree"
     />
   </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+import { useJobsStore } from '@/stores/jobs';
+
+const jobsStore = useJobsStore();
+
+const skillsSearchTerm = computed({
+  get: () => jobsStore.skillsSearchTerm,
+  set: (value: string) => jobsStore.UPDATE_SKILLS_SEARCH_TERM(value),
+});
+</script>
